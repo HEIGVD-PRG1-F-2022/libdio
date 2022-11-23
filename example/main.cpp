ce8f3bbcd81d831806ec325ed9fcc5a99dac6f23
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-11-23 17:49:00 +0100

    Format main.cpp too

--- example/main.cpp
+++ example/main.cpp
@@ -5,45 +5,45 @@
 using namespace std;
 
 int main() {
-  Display display(Display::Colors::PINK);
+    Display display(Display::Colors::PINK);
 
-  Display::showText("Test", Display::Colors::GREEN);
-  this_thread::sleep_for(1000ms);
+    Display::showText("Test", Display::Colors::GREEN);
+    this_thread::sleep_for(1000ms);
 
-  display.saveCursorPosition();
-  display.setContent("Hello World!\nteste");
-  display.show();
-  this_thread::sleep_for(1000ms);
+    display.saveCursorPosition();
+    display.setContent("Hello World!\nteste");
+    display.show();
+    this_thread::sleep_for(1000ms);
 
-  display.goBackToCursorPosition();
-  display.setTextColor(Display::Colors::BLUE);
-  display.setContent("bouh");
-  display.show();
-  this_thread::sleep_for(1000ms);
+    display.goBackToCursorPosition();
+    display.setTextColor(Display::Colors::BLUE);
+    display.setContent("bouh");
+    display.show();
+    this_thread::sleep_for(1000ms);
 
-  display.clear();
-  display.show("MAMA");
-  this_thread::sleep_for(1000ms);
+    display.clear();
+    display.show("MAMA");
+    this_thread::sleep_for(1000ms);
 
-  vector<vector<string>> grid(6, vector<string>(6, "0"));
-  display.DisplayGrid(grid, false);
-  this_thread::sleep_for(1000ms);
+    vector<vector<string>> grid(6, vector<string>(6, "0"));
+    display.DisplayGrid(grid, false);
+    this_thread::sleep_for(1000ms);
 
-  for (auto &row : grid) {
-    for (auto &item : row) {
-      item = Display::setTextColor("a", Display::Colors::PINK);
+    for (auto &row: grid) {
+        for (auto &item: row) {
+            item = Display::setTextColor("a", Display::Colors::PINK);
+        }
     }
-  }
-  display.DisplayGrid(grid);
-  this_thread::sleep_for(1000ms);
+    display.DisplayGrid(grid);
+    this_thread::sleep_for(1000ms);
 
-  int age = userInputRange<int>("Quel age as-tu ?", 0, 200, "Mauvais entrée");
+    int age = userInputRange<int>("Quel age as-tu ?", 0, 200, "Mauvais entrée");
 
-  string tmp = "Vous avez " +
-               Display::setTextColor(to_string(age), Display::Colors::RED) +
-               " ans";
-  Display::showText(tmp);
-  this_thread::sleep_for(1000ms);
+    string tmp = "Vous avez " +
+                 Display::setTextColor(to_string(age), Display::Colors::RED) +
+                 " ans";
+    Display::showText(tmp);
+    this_thread::sleep_for(1000ms);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
